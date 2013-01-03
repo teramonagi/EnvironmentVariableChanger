21e4b591f55cb6275ac351e536cff595c0af0d2d
teramonagi <teramonagi@gmail.com>
2013-01-03 19:06:35 +0900

    First commit

---
+++ EnvironmentVariableChanger/Main.fsx
@@ -0,0 +1,24 @@
+﻿open System;;
+open System.Windows.Forms;;
+
+type MainForm = class 
+    inherit Form
+
+    val box : TextBox
+
+    new() as this =
+        {
+            inherit Form()
+            box = new TextBox(Height=400)
+        }
+        then
+            this.box.ScrollBars <- ScrollBars.Vertical
+            this.box.Multiline <- true
+            this.box.Dock <- DockStyle.Top
+            let paths = Environment.GetEnvironmentVariable("PATH");
+            this.box.Text <- paths.Replace(";", Environment.NewLine)
+            this.Controls.Add(this.box)
+end;;
+
+[<STAThread>]
+do System.Windows.Forms.Application.Run(new MainForm());;
