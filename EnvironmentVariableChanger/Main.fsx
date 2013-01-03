3a0022eed212248824a5c61f030cb5efa2bd0434
teramonagi <teramonagi@gmail.com>
2013-01-03 22:16:26 +0900

    First release

--- EnvironmentVariableChanger/Main.fsx
+++ EnvironmentVariableChanger/Main.fsx
@@ -1,24 +1,46 @@
 ﻿open System;;
 open System.Windows.Forms;;
 
-type MainForm = class 
+[<Literal>]
+let HEIGHT = 400
+
+type MainForm = class
     inherit Form
-
     val box : TextBox
-
-    new() as this =
+    val ok  : Button
+    val ng  : Button
+    //
+    member this.ok_Click(_) =
+        let paths = this.box.Text.Replace(Environment.NewLine,";")
+        Environment.SetEnvironmentVariable("PATH", paths, EnvironmentVariableTarget.User)
+        this.Close()
+    member this.ng_Click(_) = 
+        this.Close()
+    //
+    new() as this = 
         {
-            inherit Form()
-            box = new TextBox(Height=400)
+            inherit Form(Width=900, Height=HEIGHT + 100, Text="Environment variable changer")
+            box = new TextBox(Height=HEIGHT)
+            ok  = new Button(Top=HEIGHT)
+            ng  = new Button(Top=HEIGHT)
         }
         then
+            //settings of objects
             this.box.ScrollBars <- ScrollBars.Vertical
             this.box.Multiline <- true
             this.box.Dock <- DockStyle.Top
-            let paths = Environment.GetEnvironmentVariable("PATH");
-            this.box.Text <- paths.Replace(";", Environment.NewLine)
+            this.ng.Left <- this.ok.Width+5
+            this.ok.Text <- "OK"
+            this.ng.Text <- "Cancel"
+            this.ok.Click.Add(this.ok_Click)
+            this.ng.Click.Add(this.ng_Click)
+            //add controls to this form
             this.Controls.Add(this.box)
+            this.Controls.Add(this.ok)
+            this.Controls.Add(this.ng)
+            //get environmental variable of user
+            let paths = Environment.GetEnvironmentVariable("PATH",EnvironmentVariableTarget.User);
+            this.box.Text <- paths.Replace(";", Environment.NewLine)
 end;;
-
 [<STAThread>]
 do System.Windows.Forms.Application.Run(new MainForm());;
---
+++ README.rd
@@ -0,0 +1,4 @@
+Environment variable changer
+=======================================
+This application gives you a simple way to change user environment variables.
+
